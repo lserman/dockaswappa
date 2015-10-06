be10fd44bc99a08c72dd6f3abdd47057b013f21a
Logan Serman <logan.serman@metova.com>
2015-10-06 11:32:08 -0500

    Log command path

--- lib/dockaswappa/docker_command.rb
+++ lib/dockaswappa/docker_command.rb
@@ -1,7 +1,9 @@
 module Dockaswappa
   module DockerCommand
     def docker(command)
-      `#{docker_absolute_command_path} #{command}`
+      command = "#{docker_absolute_command_path} #{command}"
+      logger.info "Running command: #{command}"
+      `#{command}`
     end
 
     private
